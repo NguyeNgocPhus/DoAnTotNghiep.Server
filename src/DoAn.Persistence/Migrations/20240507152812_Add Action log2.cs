d66f06f4a48be96039aac52b25a03444def89f8d
phunn <phuteobs0166@gmail.com>
2024-05-07 23:30:52 +0700

    migration

---
+++ src/DoAn.Persistence/Migrations/20240507152812_Add Action log2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoAn.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActionlog2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActionLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WorkflowDefinitionId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkflowInstanceId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContextId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActivityId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ActivityName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ActionReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateTime = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActionLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActionLogs");
+        }
+    }
+}
---
+++ src/DoAn.Persistence/Migrations/20240507153936_Add Action log3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DoAn.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddActionlog3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreateTime",
+                table: "ActionLogs",
+                newName: "CreatedTime");
+
+            migrationBuilder.RenameColumn(
+                name: "CreateBy",
+                table: "ActionLogs",
+                newName: "UpdatedBy");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedBy",
+                table: "ActionLogs",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "ActionLogs",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedTime",
+                table: "ActionLogs",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "ActionLogs");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "ActionLogs");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedTime",
+                table: "ActionLogs");
+
+            migrationBuilder.RenameColumn(
+                name: "UpdatedBy",
+                table: "ActionLogs",
+                newName: "CreateBy");
+
+            migrationBuilder.RenameColumn(
+                name: "CreatedTime",
+                table: "ActionLogs",
+                newName: "CreateTime");
+        }
+    }
+}
